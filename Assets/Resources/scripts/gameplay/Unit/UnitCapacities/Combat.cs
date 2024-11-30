e60d48e4b50daa5964912cd727c135f22e5eaa02
CarlosMartinD <youremail@yourdomain.com>
2024-11-30 18:14:56 +0100

    Add weapon triangle

--- Assets/Resources/scripts/gameplay/Unit/UnitCapacities/Combat.cs
+++ Assets/Resources/scripts/gameplay/Unit/UnitCapacities/Combat.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Combat
 {
     private Unit unit;
     private MovementEngine movementEngine;
-
 
     public Combat(Unit unit, MovementEngine movementEngine)
     {
@@ -39,7 +36,31 @@ public class Combat
         }
 
         int damage = Math.Max(0, unit.stats.attack - target.stats.deffense);
+        damage = CalculateTriangleWeaponVariation(damage, target);
         target.takeDamage(damage);
+    }
+
+    private int CalculateTriangleWeaponVariation(int damage, Unit target)
+    {
+        const float advantageBonus = 1.2f;
+        const float disadvantagePenalty = 0.8f;
+        Weapon attackerWeapon = unit.weapon;
+        Weapon targetWeapon = target.weapon;
+
+        if ((attackerWeapon == Weapon.SWORD && targetWeapon == Weapon.SWORD) ||
+            (attackerWeapon == Weapon.AXE && targetWeapon == Weapon.AXE) ||
+            (attackerWeapon == Weapon.SPIKE && targetWeapon == Weapon.SPIKE))
+        {
+            return (int)(damage * advantageBonus);
+        }
+        else if ((attackerWeapon == Weapon.SWORD && targetWeapon == Weapon.SPIKE) ||
+                 (attackerWeapon == Weapon.AXE && targetWeapon == Weapon.SWORD) ||
+                 (attackerWeapon == Weapon.SPIKE && targetWeapon == Weapon.AXE))
+        {
+            return (int)(damage * disadvantagePenalty);
+        }
+
+        return damage;
     }
 
     private bool IsEnemy(Unit otherUnit)
